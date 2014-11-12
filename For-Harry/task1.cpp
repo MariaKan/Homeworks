fce914f7e1fd77a11142e29e10ace1c4161262d0
Maria Kanazirova <19erzascarlet95@gmail.com>
2014-11-12 21:37:43 +0200

    When the electricity went out.

--- For-Harry/task1.cpp
+++ For-Harry/task1.cpp
@@ -20,18 +20,18 @@ int main()
 
 	// Sum
 	while (num != 0)
-	{ 
-		
+	{
+
 		sum += num % 10;
 		num /= 10;
 	}
 
-	cout <<"The sum of the digits is: " << sum << endl;
-	
+	cout << "The sum of the digits is: " << sum << endl;
+
 	// 3s
 	num = num_holder;
 
-	while(num != 0)
+	while (num != 0)
 	{
 		check = num % 10;
 		if (check == 3)
@@ -49,7 +49,7 @@ int main()
 	cout << "The 3s in the number are: " << threes << endl;
 
 	// digits	
-	num = num_holder; 
+	num = num_holder;
 	while (num != 0)
 	{
 		digits++;
@@ -58,35 +58,35 @@ int main()
 
 	// same
 	num = num_holder;
-	 while (num != 0)
+	while (num != 0)
 	{
 		check = num % 10;
 		check2 = num / 10 % 10;
-		 for (int i = 0; i <= digits; i++)
-		 {
-			 
-			 if (check != check2)
-			 {
-				 same = false;
-				 num /= 10;
-			 }
-			 else
-			 {
-				 same = true;
-				 num /= 10;
-			 }
+		for (int i = 0; i <= digits; i++)
+		{
 
-		 }
+			if (check != check2)
+			{
+				same = false;
+				num /= 10;
+			}
+			else
+			{
+				same = true;
+				num /= 10;
+			}
+
+		}
 	}
-	
-	 if (same)
-	 {
-		 cout << "There are same digits.\n";
-	 }
-	 else
-	 {
-		 cout << "There aren't same digits.\n";
-	 } 
+
+	if (same)
+	{
+		cout << "There are same digits.\n";
+	}
+	else
+	{
+		cout << "There aren't same digits.\n";
+	}
 
 	system("pause");
 	return 0;
